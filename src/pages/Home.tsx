a41bc3b715e6e66f9ab04cdafd6732aa1c10ecfc
GC-Park <kuyt3122@naver.com>
2024-03-11 19:22:59 +0900

    refactor: remove InterView styled component and add PlayList to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,15 @@
 import { useState } from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components';
+import PlayList from '../components/PlayList';
 
 function Home() {
   return (
     <>
       <Header />
-      <ItemView></ItemView>
+      <PlayList />
     </>
   );
 }
-
-const ItemView = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 70vw;
-  margin: 0 auto;
-`;
 
 export default Home;
