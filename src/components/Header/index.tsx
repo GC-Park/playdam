ce49b9c1d655ab7deb1d1dee104831c343491191
GC-Park <kuyt3122@naver.com>
2024-03-11 20:40:07 +0900

    refactor: modify Header Css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,14 +9,12 @@ function Header() {
 }
 
 const HeaderWrapper = styled.div`
-  width: 100%;
   height: 72px;
   background-color: #A301DB;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
-  border-bottom: 1px solid #decfcf;
 `;
 
 const HeaderLogo = styled.h1`
