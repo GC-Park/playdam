e6635349bcb6f00a79b443b049b71ee6b015afd1
GC-Park <kuyt3122@naver.com>
2024-03-12 20:30:04 +0900

    feat: add Inventory component to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components';
 import PlayList from '../components/PlayList';
+import Inventory from '../Inventory';
 
 function Home() {
   return (
     <>
       <Header />
+      <Inventory />
       <PlayList />
     </>
   );
