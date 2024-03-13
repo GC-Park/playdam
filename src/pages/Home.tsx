ad0c252e119a874767996fc897fd091d6b989b9b
GC-Park <kuyt3122@naver.com>
2024-03-13 23:42:51 +0900

    design: move Inventory component folder to component folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components';
 import PlayList from '../components/PlayList';
-import Inventory from '../Inventory';
+import Inventory from '../components/Inventory';
 
 function Home() {
   return (
