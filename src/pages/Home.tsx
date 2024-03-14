446e588fea0d534194ae0fe695f549422a7f4b4d
GC-Park <kuyt3122@naver.com>
2024-03-14 20:50:45 +0900

    refactor: delete unnecessay code in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
-import Header from '../components/Header';
-import styled from 'styled-components';
 import PlayList from '../components/PlayList';
 import Inventory from '../components/Inventory';
 
 function Home() {
   return (
     <>
-      <Header />
       <Inventory />
       <PlayList />
     </>
