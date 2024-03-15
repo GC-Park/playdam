e137c5f37c536a737a1fbc1ab5ffc8a6765fbe81
GC-Park <kuyt3122@naver.com>
2024-03-15 10:02:38 +0900

    feat: add Root component

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './Root';
 import ErrorPage from './ErrorPage';
+import Home from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [],
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+    ],
   },
 ]);
