339edf9a747bebcc822eb85b2651de0958c61468
GC-Park <kuyt3122@naver.com>
2024-03-18 17:12:18 +0900

    refactor: add ReviewCollection view css

--- src/pages/ReviewCollection.tsx
+++ src/pages/ReviewCollection.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import Inventory from '../components/Inventory';
+import styled from 'styled-components';
 
 function ReviewCollection() {
   let location = useLocation();
@@ -7,9 +8,17 @@ function ReviewCollection() {
   return (
     <>
       <Inventory path={location.pathname}/>
-      <>hello</>
+      <ReviewCollectionView></ReviewCollectionView>
     </>
   );
 }
+
+const ReviewCollectionView = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 70vw;
+  margin: 0 auto;
+`
 
 export default ReviewCollection;
