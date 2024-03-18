4ee61463196d759574986bf8e5c8d3934c538233
GC-Park <kuyt3122@naver.com>
2024-03-18 17:30:01 +0900

    feat: add PlayReviewList component

---
+++ src/components/PlayReviewList/index.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import mockData from '../mockData/playItemList.json';
+import { PlayItemProps } from '../../types/PlayItem';
+import PlayReviewItem from '../PlayReviewItem';
+
+function PlayReviewList() {
+  return (
+    <PlayReviewListWrapper>
+      {mockData.playItemList.map((Item: PlayItemProps) => (
+        <PlayReviewItem key={Item.id} id={Item.id} image={Item.image} />
+      ))}
+    </PlayReviewListWrapper>
+  );
+}
+
+const PlayReviewListWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin: 48px 0;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  grid-column-gap: 40px;
+  grid-row-gap: 48px;
+`;
+
+export default PlayReviewList;
