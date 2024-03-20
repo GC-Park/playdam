b533df9eece7c22f1c6f9ba2d360671cb7ed4da7
GC-Park <kuyt3122@naver.com>
2024-03-20 15:29:00 +0900

    feat: add PlayReviewBlogList component

---
+++ src/components/PlayReviewBlogList/index.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import mockData from '../mockData/playReviewBlogList.json';
+import { PlayReviewBlogProps } from "../../types/PlayReviewBlog";
+import PlayReviewBlogItem from "../PlayReviewBlogItem";
+
+function PlayReviewBlogList() {
+  return (
+    <PlayReviewBlogListWrapper>
+      {mockData.playReviewBlogList.map((Item: PlayReviewBlogProps) => (
+        <PlayReviewBlogItem />
+      ))}
+    </PlayReviewBlogListWrapper>
+  );
+}
+
+const PlayReviewBlogListWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin: 48px 0;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  grid-column-gap: 40px;
+  grid-row-gap: 48px;
+`;
+
+export default PlayReviewBlogList;
