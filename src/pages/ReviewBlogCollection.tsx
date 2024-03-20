dc92ae8c4c363fa56a8b886c78d7b6ac8b14dde0
GC-Park <kuyt3122@naver.com>
2024-03-20 15:33:29 +0900

    feat: add ReviewBlogCollection page

---
+++ src/pages/ReviewBlogCollection.tsx
@@ -0,0 +1,26 @@
+import { useLocation } from 'react-router-dom';
+import Inventory from '../components/Inventory';
+import styled from 'styled-components';
+import PlayReviewBlogList from '../components/PlayReviewBlogList';
+
+function ReviewBlogCollection() {
+  let location = useLocation();
+  return (
+    <>
+      <Inventory path={location.pathname} />
+      <ReviewBlogCollectionView>
+        <PlayReviewBlogList />
+      </ReviewBlogCollectionView>
+    </>
+  );
+}
+
+const ReviewBlogCollectionView = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 70vw;
+  margin: 0 auto;
+`;
+
+export default ReviewBlogCollection;
