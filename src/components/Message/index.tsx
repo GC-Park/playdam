076158c6e908840432b8138d5fed8588c2b21289
GC-Park <kuyt3122@naver.com>
2024-06-16 15:33:33 +0900

    refactor: Change the message size and apply line breaks to long sentences to fit the content within the message

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -19,23 +19,29 @@ const Message = ({ text, isUser }: any) => {
 const ChatUserMessage = styled.div`
   margin-bottom: 20px;
   max-width: 80%;
+  width: auto;
+  display: inline-block;
 
   align-self: flex-end;
   background: #2979ff;
   color: #fff;
   padding: 10px 15px;
   border-radius: 16px 16px 0 16px;
+  word-break: break-word
 `;
 
 const ChatAiMessage = styled.div`
   margin-bottom: 20px;
   max-width: 80%;
+  width: auto;
+  display: inline-block;
 
   align-self: flex-start;
   background: #f0f0f0;
   color: #333;
   padding: 10px 15px;
   border-radius: 16px 16px 16px 0;
+  word-break: break-word
 `;
 
 export default Message;
