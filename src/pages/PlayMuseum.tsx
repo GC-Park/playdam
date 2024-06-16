0ae98bdfb1ee320c437599e84087e54565b6fc62
GC-Park <kuyt3122@naver.com>
2024-06-16 13:27:39 +0900

    refactor: change css and remove unnecessary code in PlayMuseum

--- src/pages/PlayMuseum.tsx
+++ src/pages/PlayMuseum.tsx
@@ -3,33 +3,34 @@ import { useLocation } from 'react-router-dom';
 import Inventory from '../components/Inventory';
 import styled from 'styled-components';
 import example from '../threejs/Museum';
-import mockData from '../components/mockData/playItemList.json';
 
 function PlayMuseum() {
   let location = useLocation();
-  const images = mockData.playItemList.map(item => {
-    return item.poster;
-  });
 
   useEffect(() => {
     example();
   });
 
   return (
-    <>
+    <PlayMuseumWrapper>
       <Inventory path={location.pathname} />
       <PlayMuseumView>
         <PlayMuseumCanvas id="three-canvas" />
       </PlayMuseumView>
-    </>
+    </PlayMuseumWrapper>
   );
 }
+
+const PlayMuseumWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 const PlayMuseumView = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 70vw;
+  align-items: center;
   margin: 0 auto;
 `;
 
